53683b54f0f1c347a02ca2859c6082a1e588f6ab
Trung Nguyen <duc.nguyen@check24.de>
2023-11-18 15:38:16 +0100

    rescue time calculated

---
+++ script/bsc/calculateRescueTime.js
@@ -0,0 +1,32 @@
+// otherFile.js
+const {
+  getFirstTransactionAfterCreation,
+} = require("./getFirstTrxBlockByContract");
+
+// Smart contract address to query
+// MAD Incident
+const contractAddress = "0x525c8e9c8240a55014BC55cbE8908EadADB02094";
+
+// Use the exported function
+// can return them both here -> by adjust the index of smth +1 +2
+// TODO: write the rescue time to the DB or a JSON
+getFirstTransactionAfterCreation(contractAddress)
+  .then((result) => {
+    console.log("Created at Block # ");
+    console.log(result.creationTransaction.blockNumber);
+    console.log("First Transaction After Contract Creation:");
+    console.log(result.firstTransactionAfterCreation.blockNumber);
+
+    console.log("RESCUE TIME!: ");
+    console.log(
+      ((result.firstTransactionAfterCreation.blockNumber -
+        result.creationTransaction.blockNumber) *
+        3) /
+        60 /
+        60
+    );
+    console.log("Hours");
+  })
+  .catch((error) => {
+    console.error(`Error: ${error.message}`);
+  });
