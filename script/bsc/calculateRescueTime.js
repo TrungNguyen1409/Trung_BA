d99840256c3b9cca3df11db19d28acdb411e8e7e
Trung Nguyen <duc.nguyen@check24.de>
2023-11-21 03:06:21 +0100

    save db

--- script/bsc/calculateRescueTime.js
+++ script/bsc/calculateRescueTime.js
@@ -6,7 +6,7 @@ const {
 // Smart contract address to query
 // MAD Incident
 const contractAddress1 = "0x525c8e9c8240a55014BC55cbE8908EadADB02094";
-const contractAddress = "0x3463a663de4ccc59c8b21190f81027096f18cf2a";
+const contractAddress = "0x3d817ea746edd02c088c4df47c0ece0bd28dcd72";
 // Use the exported function
 // can return them both here -> by adjust the index of smth +1 +2
 // TODO: write the rescue time to the DB or a JSON
