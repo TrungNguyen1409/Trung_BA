bf94a6b8b7cd995f222dfa98f9181e5210b55da5
Trung Nguyen <duc.nguyen@check24.de>
2023-11-21 15:20:33 +0100

    adjusted

--- script/bsc/calculateRescueTime.js
+++ script/bsc/calculateRescueTime.js
@@ -6,38 +6,38 @@ const {
 const getAddressByChainType = require("./getAddressFromDB");
 
 // Smart contract address to query
-// MAD Incident
-const addressArray = getAddressByChainType("BSC")
-  .then((result) => {
-    console.log(`Adversary Contracts with chain "bsc":`, result);
-    return result;
+const contractAddress = "0xbf7fc9e12bcd08ec7ef48377f2d20939e3b4845d";
+
+// Use Promise chaining to ensure getAddressByChainType is completed before using its result
+getAddressByChainType("BSC")
+  .then((addressArray) => {
+    console.log(`Adversary Contracts with chain "BSC":`, addressArray);
+
+    // Use the exported function
+    // can return them both here -> by adjusting the index of something +1 +2
+    // TODO: write the rescue time to the DB or a JSON
+
+    getFirstTransactionAfterCreation(contractAddress)
+      .then((result) => {
+        console.log(
+          `Created at Block: ${result.creationTransaction.blockNumber}`
+        );
+        console.log(
+          `First Transaction After Contract Creation at Block: ${result.firstTransactionAfterCreation.blockNumber}`
+        );
+
+        console.log(
+          `RESCUE TIME!: ${
+            (result.firstTransactionAfterCreation.blockNumber -
+              result.creationTransaction.blockNumber) *
+            3
+          } seconds`
+        );
+      })
+      .catch((error) => {
+        console.error(`Error: ${error.message}`);
+      });
   })
   .catch((error) => {
     console.error("Error:", error);
   });
-const contractAddress1 = "0x525c8e9c8240a55014BC55cbE8908EadADB02094";
-const contractAddress = "0xbf7fc9e12bcd08ec7ef48377f2d20939e3b4845d";
-// Use the exported function
-// can return them both here -> by adjust the index of smth +1 +2
-// TODO: write the rescue time to the DB or a JSON
-
-getFirstTransactionAfterCreation(contractAddress)
-  .then((result) => {
-    console.log(`Created at Block: ${result.creationTransaction.blockNumber}`);
-    console.log(
-      `First Transaction After Contract Creation at Block: ${result.firstTransactionAfterCreation.blockNumber}`
-    );
-
-    console.log(
-      `RESCUE TIME!: ${
-        (result.firstTransactionAfterCreation.blockNumber -
-          result.creationTransaction.blockNumber) *
-        3
-      } seconds`
-    );
-  })
-  .catch((error) => {
-    console.error(`Error: ${error.message}`);
-  });
-
-console.log(addressArray);
