b35f4142099b122e428a1db85a5f3741e1c3a198
Trung Nguyen <duc.nguyen@check24.de>
2023-11-24 22:52:03 +0100

    remove comment

--- automated_ityfuzz/run_ityFuzz.js
+++ automated_ityfuzz/run_ityFuzz.js
@@ -3,13 +3,11 @@ const util = require("util");
 const exec = util.promisify(require("child_process").exec);
 const fsExtra = require("fs-extra");
 
-// Read the JSON file
 const jsonString = fs.readFileSync("exact_block_exploits.json", "utf8");
 const tasks = JSON.parse(jsonString);
 
 const errorLogFile = "error_log.txt";
 
-// Function to execute ityfuzz command with a timeout
 async function executeItyfuzzCommandWithTimeout(
   command,
   timeout,
@@ -37,14 +35,12 @@ async function executeItyfuzzCommandWithTimeout(
       console.log("Command executed successfully. Output:");
       console.log(stdout);
 
-      // Check if the "work_dir" folder exists before copying
       if (fs.existsSync("work_dir")) {
-        // Copy the "work_dir" folder to a new folder named after the current execution
         const destinationFolder = `result_test/${executionName}`;
+
         fsExtra.copySync("work_dir", destinationFolder);
         console.log(`Copied "work_dir" to "${destinationFolder}"`);
 
-        // Remove the "work_dir" folder
         fsExtra.removeSync("work_dir");
         console.log('Removed "work_dir"');
       } else {
@@ -58,7 +54,6 @@ async function executeItyfuzzCommandWithTimeout(
         `${executionName} : ${error.message}\n`
       );
       if (fs.existsSync("work_dir")) {
-        // Remove the "work_dir" folder
         fsExtra.removeSync("work_dir");
         console.log('Removed "work_dir"');
       } else {
@@ -70,9 +65,9 @@ async function executeItyfuzzCommandWithTimeout(
   return Promise.race([executionPromise, timeoutPromise]);
 }
 
-// Function to run tasks with a timeout
 async function runTasksWithTimeout(timeout) {
   console.log(tasks);
+
   for (const task of tasks) {
     console.log(`Running task: ${task.name}`);
 
